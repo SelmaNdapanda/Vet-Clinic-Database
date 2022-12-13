67ffe42434cff8d2ef258487fdeac84e51accef7
Selma <selmandapanda@yahoo.com>
2022-12-13 22:39:25 +0200

    Add transactions

--- queries.sql
+++ queries.sql
@@ -9,4 +9,59 @@ SELECT * FROM animals WHERE neutered IS true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.4  AND weight_kg <= 17.3;
 
+-- Update the animals table by setting the species column to unspecified, then roll back
+BEGIN;
+
+UPDATE animals 
+SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+BEGIN;
+
+UPDATE animals 
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals 
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+-- delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Delete all animals born after Jan 1st, 2022.
+-- Create a savepoint for the transaction.
+-- Update all animals' weight to be their weight multiplied by -1.
+-- Rollback to the savepoint
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT born_before_Jan2022;
+UPDATE animals 
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO born_before_Jan2022;
+
+UPDATE animals 
+SET weight_kg = weight_kg * -1 
+WHERE weight_kg < 0;
+
+COMMIT;
+SELECT * FROM animals;
 
