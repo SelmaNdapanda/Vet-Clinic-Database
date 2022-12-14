2f055e85fb94e3a2c0d7684988fda60d0e3e9359
Selma <selmandapanda@yahoo.com>
2022-12-14 16:59:44 +0200

    Insert data into owners and species table

--- data.sql
+++ data.sql
@@ -16,3 +16,17 @@ VALUES
 ('Boarmon', '2005-06-07', 7, 'TRUE', 20.4),
 ('Blossom', '1998-10-13', 3, 'TRUE', 17),
 ('Ditto', '2022-05-14', 4, 'TRUE', 22);
+
+INSERT INTO owners (full_name, age) 
+VALUES 
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) 
+VALUES 
+('Pokemon'),
+('Digimon');
