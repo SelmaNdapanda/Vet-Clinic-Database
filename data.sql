5d73ddb2354e13646fc7342d1acfb8d07df09444
Selma <selmandapanda@yahoo.com>
2022-12-15 16:48:14 +0200

    Insert data for vets

--- data.sql
+++ data.sql
@@ -62,4 +62,12 @@ SET owner_id =
 UPDATE animals
 SET owner_id = 
     (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
-    WHERE name IN ('Angemon', 'Boarmon');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    WHERE name IN ('Angemon', 'Boarmon'); 
+
+-- Insert data for vets
+INSERT INTO vets (name, age, date_of_graduation) 
+VALUES 
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
